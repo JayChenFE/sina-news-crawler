99f9fdfc17aa40cd175b91baf9539447747d0b3c
JayChenFE <jaychenfe@foxmail.com>
2020-02-06 10:54:41 +0800

    使用多线程

--- src/main/java/com/github/jaychenfe/Main.java
+++ src/main/java/com/github/jaychenfe/Main.java
@@ -3,7 +3,7 @@ package com.github.jaychenfe;
 public class Main {
     public static void main(String[] args) {
         CrawlerDao dao = new MyBatisCrawlerDao();
-        for (int i = 0; i <8 ; i++) {
+        for (int i = 0; i < 8; i++) {
             new Crawler(dao).start();
         }
     }
