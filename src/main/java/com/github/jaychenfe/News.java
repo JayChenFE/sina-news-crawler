6f308c64c063b9a0167e1cc5fc1b02fc6c5acf22
JayChenFE <jaychenfe@foxmail.com>
2020-02-07 22:07:08 +0800

    mock 数据

--- src/main/java/com/github/jaychenfe/News.java
+++ src/main/java/com/github/jaychenfe/News.java
@@ -1,16 +1,20 @@
 package com.github.jaychenfe;
 
+import java.time.Instant;
+
 public class News {
     private Long Id;
     private String url;
+    private String content;
+    private String title;
+    private Instant createdAt;
+    private Instant modifiedAt;
 
     public News(String url, String content, String title) {
         this.url = url;
         this.content = content;
         this.title = title;
     }
-
-    private String content;
 
     public Long getId() {
         return Id;
@@ -44,7 +48,21 @@ public class News {
         this.title = title;
     }
 
-    private String title;
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(Instant modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
 
 
 }
